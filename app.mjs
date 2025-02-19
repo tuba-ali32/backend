78c3aa56c8126d0b8dd90d7926f8b3e7071575ac
tubaali-32 <tubaali3167@gmail.com>
2025-02-19 09:20:07 +0500

    done

---
+++ app.mjs
@@ -0,0 +1,25 @@
+ import express from "express"
+const app = express()
+const port = process.env.PORT ||  3000
+
+app.get('/', (req, res) => {
+  res.send('Hello dsfhjkdsfh')
+})
+
+
+app.get('/weather', (req, res) => {
+  res.json({ user: 'tobi' })
+})
+app.get('/weather/:cityName', (req, res) => {
+  res.send(req.params)
+})
+app.get('/weather/:cityName/:side', (req, res) => {
+  res.send(req.params)
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
